6e728bbc313408a550ba14d500084f01c094b43e
Allison Williams <allisonwilliams@Allisons-MacBook.local>
2018-04-06 21:40:24 -0400

    moment logic working

--- javascript.js
+++ javascript.js
@@ -21,61 +21,82 @@ $("#add-train").on("click", function(event) {
     //grab values from text boxes
     var trainName = $("#train-name").val().trim();
     var cityName = $("#city-name").val().trim();
-    var trainTime = $("#train-time").val().trim();
     var howOften = $("#how-often").val().trim();
+    var trainTime = $("#train-time").val().trim();
+    
 
     //send firebase the info
     database.ref().push({
         trainName: trainName,
         cityName: cityName,
-        trainTime: trainTime,
         howOften: howOften,
+        trainTime: trainTime,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
 
 });
 
+//=====get the info back from firebase & display it on the page========
+
+
 //this is watching for when something is added to firebase
 database.ref().on("child_added", function(snapshot) {
 
-    //check to make sure things are coming out of firebase
-    // console.log(snapshot.val());
-    // console.log(snapshot.val().trainName);
-    // console.log(snapshot.val().cityName);
-    // console.log(snapshot.val().trainTime);
-    // console.log(snapshot.val().howOften);
-
     //change the HTML to reflect the updates
-    // $("#added-name").text(snapshot.val().trainName);
-    // $("#added-city").text(snapshot.val().cityName);
-    // $("#added-time").text(snapshot.val().trainTime);
-    // $("#added-frequency").text(snapshot.val().howOften);
-
     var sv = snapshot.val();
 
     //create new row and new cell once data is entered for train name
-    console.log(sv.trainName);
     var createRow = $("<tr></tr>");
     var createCell = $("<td></td>").append(sv.trainName);
     createRow.append(createCell);
 
     //create new cell once data is entered for destination city
-    console.log(sv.cityName);
     createCell = $("<td></td>").append(sv.cityName);
     createRow.append(createCell);
-
-    //create new cell once data is entered for train time
-    console.log(sv.trainTime);
-    createCell = $("<td></td>").append(sv.trainTime);
-    createRow.append(createCell);
-
+   
+//===========================calculate next arrival==========================    
+    
     //create new cell once data is entered for frequency
-    console.log(sv.howOften);
+    var tFrequency = sv.howOften;
     createCell = $("<td></td>").append(sv.howOften);
     createRow.append(createCell);
 
-    $("#train-table").append(createRow);
+    //variable to store first train time
+    var firstTime = sv.trainTime;
+    
+    //first time pushed back one year to make sure it comes before current time to avoid confusion
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
 
+    //current time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    //difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    //time apart(remainder)
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+    //minute until train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    //next train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+    createCell = $("<td></td>").append(sv.trainTime);
+    createRow.append(createCell);
+
+//===========================calculate minutes away==========================  
+
+    
+
+    //add newly formed row to the bottom of the table
+    $("#train-table").append(createRow);
 
 //handles errors
 }, function(errorObject) {
@@ -84,6 +105,5 @@ database.ref().on("child_added", function(snapshot) {
 
 
 
-//get the info back from firebase & display it on the page
 
 //time functionality
